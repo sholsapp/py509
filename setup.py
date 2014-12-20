1e90287cfbc5ebbc455c9fe315c1f4f90941553a
Stephen Holsapple <sholsapp@gmail.com>
2014-12-20 10:53:22 -0800

    Adjust homepage link to point at github repo.

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from setuptools import setup
 setup(
   name='py509',
   version='0.0.3',
-  description="Framework and utility code for running public key infrastructure at home.",
+  description="Framework and utility code for running public key infrastructure.",
   author='Stephen Holsapple',
   author_email='sholsapp@gmail.com',
-  url='http://www.google.com',
+  url='https://github.com/sholsapp/py509',
   packages=['py509', 'py509.asn1'],
   install_requires=[
     'certifi',
