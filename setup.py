93217bc8e074b7cec209c9b911035ceac42dcc9a
Stephen Holsapple <sholsapp@gmail.com>
2015-01-31 21:52:45 -0800

    Add a setuptools command to build scripts
    Added a pexify command that will dump all of the entry points specified
    in the setuptools object as standalone pex executables. These can be
    shipped around to wherever you need to run them!
    
    Awesome!

--- setup.py
+++ setup.py
@@ -1,8 +1,48 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+
+
+entry_points = {
+  'console_scripts': [
+    'pyssl-diff = py509.bin.diff:main',
+    'pyssl-get = py509.bin.get:main',
+    'pyssl-ls = py509.bin.ls:main',
+    'pyssl-verify = py509.bin.verify:main',
+  ],
+}
+
+
+class Pex(Command):
+
+  user_options = []
+
+  def initialize_options(self):
+    """Abstract method that is required to be overwritten"""
+
+  def finalize_options(self):
+    """Abstract method that is required to be overwritten"""
+
+  def run(self):
+    if not os.path.exists('dist/wheel-cache'):
+      print 'You need to create dist/wheel-cache first! You\'ll need to run the following.'
+      print '  mkdir dist/wheel-cache'
+      print '  pip wheel -w dist/wheel-cache'
+      return
+    for entry in entry_points['console_scripts']:
+      name, call = tuple([_.strip() for _ in entry.split('=')])
+      print 'Creating {0} as {1}'.format(name, call)
+      subprocess.check_call([
+        'pex',
+        '-r', 'py509',
+        '--no-pypi',
+        '--repo=dist/wheel-cache',
+        '-o', name,
+        '-e', call])
+
 
 setup(
   name='py509',
@@ -21,15 +61,9 @@ setup(
     'python-dateutil',
     'tabulate',
   ],
-  tests_require = [
+  tests_require=[
     'pytest',
   ],
-  entry_points = {
-    'console_scripts': [
-      'pyssl-diff = py509.bin.diff:main',
-      'pyssl-get = py509.bin.get:main',
-      'pyssl-ls = py509.bin.ls:main',
-      'pyssl-verify = py509.bin.verify:main',
-    ],
-  },
+  entry_points=entry_points,
+  cmdclass={'pexify': Pex},
 )
