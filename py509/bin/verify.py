ac744c9e9033f552f7f08dd5837139bc108f1af8
Stephen Holsapple <sholsapp@gmail.com>
2015-06-10 19:12:44 -0700

    Initial implementation of ssl-verify
    Currently, depends on trust store being fully populated and doesn't
    append intermediate trust stores to bundle before verification check.

--- py509/bin/verify.py
+++ py509/bin/verify.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 
 """Verify a certificate."""
+
+import argparse
+import logging
+import sys
+
+import certifi
+from OpenSSL import crypto
+
+from py509.x509 import load_x509_certificates
+
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def main():
+
+  trust_store = []
+  with open(certifi.where()) as fh:
+  #with open('/Users/sholsapp/workspace/py509/test.pem') as fh:
+    trust_store = list(load_x509_certificates(fh.read()))
+
+  x509store = crypto.X509Store()
+  for ca in trust_store:
+    print ca.get_subject()
+    x509store.add_cert(ca)
+
+  x509cert = crypto.load_certificate(crypto.FILETYPE_PEM, sys.stdin.read())
+
+  try:
+    crypto.X509StoreContext(x509store, x509cert).verify_certificate()
+    print 'Success'
+  except crypto.X509StoreContextError as e:
+    print 'Failed on {0}'.format(e.certificate.get_subject())
+    print 'Issuer {0}'.format(e.certificate.get_issuer())
+    print 'Message: {0}'.format(e)
