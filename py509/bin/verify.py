b97a62cc130f2396b0b62ed70be23cad8855e795
Stephen Holsapple <sholsapp@gmail.com>
2015-06-11 19:26:49 -0700

    Clean up the verify functionality a little bit

--- py509/bin/verify.py
+++ py509/bin/verify.py
@@ -16,6 +16,9 @@ from py509.x509 import load_x509_certificates
 from pyasn1.codec.der.decoder import decode
 
 
+logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
@@ -28,21 +31,34 @@ def decode_authority_information_access(asn1_data):
   :param asn1_data: The ASN.1 data to decode.
 
   """
-  OCSP = '1.3.6.1.5.5.7.48.1'
-  CA_ISSUER = '1.3.6.1.5.5.7.48.2'
+  OCSP_OID = '1.3.6.1.5.5.7.48.1'
+  CA_ISSUER_OID = '1.3.6.1.5.5.7.48.2'
   for authority in decode(asn1_data, asn1Spec=AuthorityInfoAccess()):
     if isinstance(authority, AuthorityInfoAccess):
       for entry in range(len(authority)):
         component = authority.getComponentByPosition(entry)
-        if component.getComponentByName('accessMethod').prettyPrint() == CA_ISSUER:
+        if component.getComponentByName('accessMethod').prettyPrint() == CA_ISSUER_OID:
           return component.getComponentByName('accessLocation').getComponent().asOctets()
+
+
+def get_certificate(url, strict_compliance=False):
+  http = urllib3.PoolManager()
+  rsp = http.request('GET', url, headers={'Content-Type': 'application/pkix-cert'})
+  if rsp.status == 200:
+    if strict_compliance and 'application/x-x509-ca-cert' not in rsp.headers:
+      # This web server isn't following the RFC
+      return
+  else:
+    raise RuntimeError('Failed to fetch intermediate certificate at {0}!'.format(access))
+  return crypto.load_certificate(crypto.FILETYPE_ASN1, rsp.data)
 
 
 def main():
 
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument('--ca', required=False, default=certifi.where())
-  parser.add_argument('--no-resolve-intermediate', action='store_false', default=False)
+  parser.add_argument('--no-resolve-intermediate', action='store_true', default=False)
+  parser.add_argument('--strict-compliance', action='store_true', default=False)
   args = parser.parse_args()
 
   trust_store = []
@@ -60,19 +76,11 @@ def main():
       ext = x509cert.get_extension(idx)
       if ext.get_short_name() in ['authorityInfoAccess']:
         access = decode_authority_information_access(ext.get_data())
-        http = urllib3.PoolManager()
-        rsp = http.request('GET', access)
-        if rsp.status == 200:
-          der = rsp.data
-        else:
-          raise RuntimeError('Failed to fetch intermediate certificate at {0}!'.format(access))
-        intermediate = crypto.load_certificate(crypto.FILETYPE_ASN1, der)
-        print 'Adding {0}'.format(intermediate)
-        x509store.add_cert(intermediate)
+        x509store.add_cert(get_certificate(access, strict_compliance=args.strict_compliance))
 
   try:
     crypto.X509StoreContext(x509store, x509cert).verify_certificate()
-    print 'Success'
+    print 'Good'
   except crypto.X509StoreContextError as e:
     print 'Failed on {0}'.format(e.certificate.get_subject())
     print 'Issuer {0}'.format(e.certificate.get_issuer())
