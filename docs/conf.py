60dad560631a8696f626b59f7bb166c6e060e9ea
Stephen Holsapple <sholsapp@gmail.com>
2015-06-12 18:38:18 -0700

    Add version

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ copyright = u"2001-{0}, {1}".format(datetime.date.today().year, authors)
 # built documents.
 #
 # The short X.Y version.
-version = '0.0'
+version = '0.0.15'
 # The full version, including alpha/beta/rc tags.
 release = version
 
